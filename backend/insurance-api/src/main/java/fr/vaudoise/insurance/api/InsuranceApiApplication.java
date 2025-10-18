e6c1d6494b28291dfb9f62f89d0d2d277b887be2
damsrv <40205823+damsrv@users.noreply.github.com>
2025-10-18 12:50:13 +0200

    feat(backend): Ajout d'un projet spring boot généré par spring initializr.
    - Création d'un contrôleur.
    - Démarrage du projet et des conteneurs OK.

---
+++ backend/insurance-api/src/main/java/fr/vaudoise/insurance/api/InsuranceApiApplication.java
@@ -0,0 +1,13 @@
+package fr.vaudoise.insurance.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceApiApplication.class, args);
+	}
+
+}
---
+++ backend/insurance-api/src/test/java/fr/vaudoise/insurance/api/InsuranceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.vaudoise.insurance.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
